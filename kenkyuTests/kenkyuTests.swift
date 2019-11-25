09ffbfd8192dd0fa79f4b0719e002f5c9abf64cb
Yuta Akatsu <yuta07122016@gmail.com>
2019-11-25 12:05:52 +0900

    Initial Commit

---
+++ kenkyuTests/kenkyuTests.swift
@@ -0,0 +1,34 @@
+//
+//  kenkyuTests.swift
+//  kenkyuTests
+//
+//  Created by yuta akatsu on 2019/11/25.
+//  Copyright © 2019 yuta akatsu. All rights reserved.
+//
+
+import XCTest
+@testable import kenkyu
+
+class kenkyuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
